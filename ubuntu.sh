0a2481fb8ed0da734233c5c14bbb8adb2608a0d8
Tute Costa <tutecosta@gmail.com>
2020-04-20 19:50:03 -0300

    Setting up Ubuntu in mbp 2015
    20.04 LTS. It went better than ever.

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,39 +1,49 @@
 # CLI utilities
 sudo apt update
-sudo apt install git gitk silversearcher-ag vim ctags tmux tmate curl net-tools openssh-server
-sudo apt install gnome-tweak-tool # Map: caps to ctrl, Compose Key: Right Alt
+sudo apt install git gitk silversearcher-ag vim ctags tmux tmate curl \
+  net-tools openssh-server rcm zsh gnome-tweak-tool
+# Map: caps to ctrl, Compose Key: Right Alt for Spanish accents
+# Go to Firefox Menu, Customize, uncheck "Title Bar" to hide it for more vertical space
 
 # zsh, dotfiles
-sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
-sudo apt install rcm zsh
 chsh -s $(which zsh)
-git clone git@github.com:thoughtbot/dotfiles.git ~/Sites/tb-dotfiles
-git clone git@github.com:tute/dotfiles.git ~/Sites/dotfiles
-env RCRC=$HOME/Sites/dotfiles/rcrc rcup
-# logout and log back in
+git clone git@github.com:thoughtbot/dotfiles.git ~/Sites/opensource/tb-dotfiles
+git clone git@github.com:tute/dotfiles.git ~/Sites/opensource/dotfiles
+env RCRC=$HOME/Sites/opensource/dotfiles/rcrc rcup
+# Log out and log back in for some changes to take effect
 
 # asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.0
 echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
-echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+# echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 
 # Ruby
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-sudo apt install automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
-asdf install ruby 2.5.5
-asdf global ruby 2.5.5
+sudo apt install automake autoconf libreadline-dev libncurses-dev libssl-dev \
+  libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev zlib1g-dev
+asdf install ruby 2.6.6
+asdf global ruby 2.6.6
 
 # PostgreSQL, redis
 sudo apt install redis-server postgresql postgresql-contrib libpq-dev
+sudo -u postgres -i
+CREATE ROLE tute superuser;
+CREATE USER tute;
+ALTER ROLE tute WITH LOGIN;
 
-# Qt5.5
-sudo apt install qt5-default libqt5webkit5-dev gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-x
+# Qt5.5, capybara-webkit
+sudo apt install qt5-default libqt5webkit5-dev gstreamer1.0-plugins-base \
+  gstreamer1.0-tools gstreamer1.0-x
 
 # Node, Yarn
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
-asdf install nodejs 10.1.0
-asdf global nodejs 10.1.0
+asdf install nodejs 12.16.2
+asdf global nodejs 12.16.2
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn
+
+# Apps to install manually: Slack, Sublime Text, Aptible Toolbelt, 1Password X
+# Set Slack to open links in normal browser (about:profiles)
+# Facetime camera doesn't work; https://github.com/patjak/bcwc_pcie isn't stable
