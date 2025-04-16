579aa138b619e9674f0a050fd8cd2aaa178a86a1
Tute Costa <tutecosta@gmail.com>
2025-04-16 06:05:00 -0300

    mise replaces asdf

--- ubuntu.sh
+++ ubuntu.sh
@@ -12,17 +12,8 @@ git clone git@github.com:tute/dotfiles.git ~/Code/opensource/dotfiles
 env RCRC=$HOME/Code/opensource/dotfiles/rcrc rcup
 # Log out and log back in for some changes to take effect
 
-# asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.0
-echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
-# echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
-
-# Ruby
-asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-sudo apt install automake autoconf libreadline-dev libncurses-dev libssl-dev \
-  libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev zlib1g-dev
-asdf install ruby 2.6.6
-asdf global ruby 2.6.6
+# mise manage languages versions
+# mise install in projects
 
 # PostgreSQL, redis
 sudo apt install redis-server postgresql postgresql-contrib libpq-dev
@@ -30,15 +21,6 @@ sudo -u postgres -i
 CREATE ROLE tute superuser;
 CREATE USER tute;
 ALTER ROLE tute WITH LOGIN;
-
-# Node, Yarn
-asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
-asdf install nodejs 12.16.2
-asdf global nodejs 12.16.2
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install yarn
 
 # Apps to install manually: Slack, VS Code, 1Password X
 # Set Slack to open links in normal browser (about:profiles)
